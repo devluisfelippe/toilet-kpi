1d8ff49356b9bd8697ba6b8cbca9f6b8a09eb1c5
Andrei Schopf <andreischopf@gmail.com>
2026-05-30 17:19:34 -0300

    feat(backend): add Auth, Users, and Missions modules to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CassandraModule } from './cassandra/cassandra.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MissionsModule } from './missions/missions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CassandraModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CassandraModule,
+    AuthModule,
+    UsersModule,
+    MissionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

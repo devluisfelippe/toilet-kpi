53b16fc330a10272ea11f7313cc698e533269184
Luis Felippe <luisfelippedev@outlook.com>
2026-05-30 22:25:46 -0300

    fix: new image build Dockerfile backend (#16)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,19 +1,23 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly auth: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
   register(@Body() registerDto: RegisterDto) {
     return this.auth.register(registerDto.nickname, registerDto.senha);
   }
 
   @Post('login')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Login a user' })
   login(@Body() loginDto: LoginDto) {
     return this.auth.login(loginDto.nickname, loginDto.senha);
   }
--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({ description: 'The nickname of the user' })
   @IsString({ message: 'Seu nick de cagador precisa ser string' })
   nickname!: string;
 
+  @ApiProperty({ description: 'The password of the user', minLength: 10 })
   @IsString()
   @MinLength(10, {
     message:
